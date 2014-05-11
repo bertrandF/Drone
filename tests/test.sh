60cde61e6f3766b72ee0f869d123d1017dfd8c59
bertrandF <bertrand.feuillerat@hotmail.fr>
2014-05-11 16:11:41 +0200

    Added test1 action to test script: launch central station + fill db + start rtsp servers + start dummy drone (ncat).

--- tests/test.sh
+++ tests/test.sh
@@ -22,6 +22,14 @@
 ##########################################################################
 SCRIPTDIR=`dirname $0`
 DBDIR="$SCRIPTDIR/../Database/"
+BUILDDIR="$SCRIPTDIR/../../build-Drone-Desktop-Debug/"
+
+COMMANDSTATION="$BUILDDIR/CommandStation/CommandStation"
+CENTRALSTATION="$BUILDDIR/CentralStation/CentralStation"
+
+DBNAME="drones"
+DBUSER="dronedbmanager"
+
 
 
 usage() 
@@ -29,10 +37,16 @@ usage()
 	echo "Test launching script for Drone project."
 	echo "Usage: test.sh <testname> [options]"
 	echo ""
-	echo "Options:"
-	echo ""
+	echo "Actions:"
+	echo "  db        Manage drone database."
+	echo "  test1     Run test with setup 1."
 }
 
+
+
+##############
+###   DB   ###
+##############
 usage_db() 
 {
 	echo "Manage drone database."
@@ -42,7 +56,7 @@ usage_db()
 	echo "  create    Executes create.sql script."
 	echo "  drop      Executes drop.sql script."
 	echo "  dump      Dumps tables."
-	echo "  fill      Executes dummy.sql script."
+	echo "  fill <n>  Executes script the Nth dummy script."
 	echo "  help      Prints help this message."
 	echo "  inter     Interactive psql session."
 	echo "  rebuild   Executes drop,create,fill."
@@ -50,11 +64,8 @@ usage_db()
 
 test_db()
 {
-	DBNAME="drones"
-	DBUSER="dronedbmanager"
 	DBCREATEFILE="$DBDIR/create.sql"
 	DBDROPFILE="$DBDIR/drop.sql"
-	DBFILLFILE_1="$SCRIPTDIR//dummy.sql"
 	
 	if (($# < 2)); then
 		echo "Bad number of arguments."
@@ -76,7 +87,12 @@ test_db()
 		echo "/**** logs ****/"
 		psql -d $DBNAME -U $DBUSER -c 'SELECT * FROM logs'
 	elif [ "$2" == "fill" ]; then
-		psql -d $DBNAME -U $DBUSER -f $DBFILLFILE_1
+		if (($# < 3)); then
+			echo "FILL: Bad number of arguments."
+			usage_db
+			exit
+		fi
+		psql -d $DBNAME -U $DBUSER -f "$SCRIPTDIR/dummy$3.sql"
 	elif [ "$2" == "help" ]; then
 		usage_db
 	elif [ "$2" == "inter" ]; then
@@ -90,8 +106,84 @@ test_db()
 		usage_db
 	fi
 	
-	exit
 }
+
+
+
+##############
+### TEST 1 ###
+##############
+usage_test1() 
+{
+	echo "Run test with setup 1."
+	echo "Usage: test.sh test1 <action> [options]"
+	echo ""
+	echo "Actions:"
+	echo "  help      Prints this help."
+	echo "  run       Run the test."
+	
+}
+
+test_test1()
+{
+	if (($# < 2)); then
+		echo "Bad number of arguments."
+		usage_test1
+		exit
+	fi
+	
+	
+	if [ "$2" == "help" ]; then
+		usage_test1
+	elif [ "$2" == "run" ]; then
+		LOCALIP=`ip -4 -o address show wlp3s0 | \
+								egrep --color -o '([0-9]+\.){3}[0-9]+' | \
+								head -n1`
+		
+		# CREATE DB
+		test_db db drop 
+		test_db db create
+		
+		# START CENTRAL STATION and WAIT FOR START
+		xterm -geometry 100x30+0+0 -e $CENTRALSTATION $LOCALIP 5866 &
+		PIDCENTRAL=$!
+		sleep 2
+		
+		# START DUMMY RTSP SERVERS
+		STREAMFILE1="/home/bertrand/DATA/Movies/Goodfellas[1990][Special.Edition]DvDrip[Eng]-Zeus_Dias.avi"
+		RTSP1="rtsp://$LOCALIP:8080/frontVideo.sdp"
+		vlc --quiet $STREAMFILE1 --sout "#rtp{sdp=$RTSP1}" &
+		PIDVLC1=$!
+		
+		STREAMFILE2="/home/bertrand/DATA/Movies/28.jours.Plus.tard.avi"
+		RTSP2="rtsp://$LOCALIP:8081/frontVideo.sdp"
+		vlc --quiet $STREAMFILE1 --sout "#rtp{sdp=$RTSP2}" &
+		PIDVLC2=$!
+		
+		# FILL DB WITH DUMMIES
+		psql -d $DBNAME -U $DBUSER -c "INSERT INTO stations (type, ip, port, info) VALUES ('drone', '$LOCALIP', 5867, 'UAV-00237XFT01')" 1> /dev/null
+		psql -d $DBNAME -U $DBUSER -c "INSERT INTO videos (id, videos) VALUES (1, '$STREAMFILE1\$$STREAMFILE2')" 1> /dev/null
+		psql -d $DBNAME -U $DBUSER -c "INSERT INTO sessions (station1, station2) VALUES (0, 1)" 1> /dev/null
+		
+		# START DUMMY DRONE
+		xterm -geometry 100x30+0+500 -e ncat -lu $LOCALIP 5867 &
+		PIDDRONE=$!
+		
+		# START COMMAND STATION and WAIT EXIT
+		$COMMANDSTATION
+		
+		# CLEAN
+		kill $PIDCENTRAL
+		kill $PIDDRONE
+		kill $PIDVLC1
+		kill $PIDVLC2
+	fi
+}
+
+
+
+
+
 
 # Check that we got at least a testname
 if (($# < 1)); then 
@@ -103,4 +195,6 @@ TESTNAME=$1
 # Call corresponding script
 if [ "$TESTNAME" == "db" ]; then
 	test_db $@	
+elif [ "$TESTNAME" == "test1" ]; then
+	test_test1 $@
 fi
