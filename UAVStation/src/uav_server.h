b46387bea18c4955c9961ab6334b03cfe2cc324a
bertrandF <bertrand.feuillerat@hotmail.fr>
2014-05-12 17:36:57 +0200

    Added file uav_server.h.

---
+++ UAVStation/src/uav_server.h
@@ -0,0 +1,36 @@
+/*!
+ *   \file  uav_server.h
+ *   \brief  uav_server.c include file.
+ *  
+ *  uav_server.c function exports.
+ *  
+ *  \author  Bertrand.F (), 
+ *  
+ *  \internal
+ *       Created:  12/05/2014
+ *      Revision:  none
+ *      Compiler:  gcc
+ *  Organization:  
+ *     Copyright:  Copyright (C), 2014, Bertrand.F
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __UAV_SERVER_H__
+#define __UAV_SERVER_H__
+
+extern void uavsrv_run(int sock);
+
+#endif
