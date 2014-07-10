0ebb00faada05f9fb2ce6677edf9e2e172c6c4a9
bertrandF <bertrand.feuillerat@hotmail.fr>
2014-07-10 21:00:28 +0200

    Added python script to configure db, launch central server, start UAVStation.

---
+++ tests/run-central-uav.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2.7
+
+import sys
+import os
+
+
+def run():
+    os.system("./tests/test.sh db drop")
+    os.system("./tests/test.sh db create")
+    
+    pid = os.fork()
+    if pid == 0:
+        os.execl("../build-Drone-Desktop-Debug/CentralStation/CentralStation", ("127.0.0.1" "5688"))
+        sys.exit()
+    else:
+        print "CentralStation PID: ", str(pid)
+
+    os.system("./UAVStation/bin/Debug/UAVStation ./UAVStation/conf.lua")
+
+
+try:
+    run()
+except:
+    pass
+finally:
+    print "Killing processes ..."
+    os.system("killall CentralStation")
+    os.system("killall UAVStation")
